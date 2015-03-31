70e2aaebf39053a5de758b70adfbc05205ddc856
Nick Schuch <nick@myschuch.com>
2015-03-31 15:24:49 -0700

    Update build scripts so we don't install the results site on deployment.

--- packer/scripts/install.sh
+++ packer/scripts/install.sh
@@ -4,6 +4,16 @@
 # Description: Install the results site.
 
 mv /tmp/results /var/www/results
-cd /var/www/results && phing build
+
+cd /var/www/results && phing make
+
+mkdir -p /var/www/results/sites/default/files/tmp
+mkdir -p /var/www/results/sites/default/private
+
 chown -R www-data:www-data /var/www/results
 
+# This adds a line to ensure we are loading configuration from
+# the file shipped with the parent image.
+if [ -f /etc/drupalci/settings.local.php ]; then
+    echo "require_once /etc/drupalci/settings.local.php" >> /var/www/results/app/sites/default/settings.php
+fi
