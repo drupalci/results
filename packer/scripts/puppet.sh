c37992e9922c5087c22a1c9a34fbed3109ec691a
Rudy Grigar <basic@drupal.org>
2015-04-08 18:19:48 -0700

    Move all data to Drupal.org - https://www.drupal.org/project/drupalci_results

--- packer/scripts/puppet.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -eux
-
-# Name:        puppet.sh
-# Author:      Nick Schuch (nick@myschuch.com)
-# Description: Installs Puppet manifest.
-
-DIR='/tmp/results/puppet'
-
-cd $DIR && sh scripts/provision.sh
--- puppet/scripts/vagrant.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Script: vagrant.sh
-# Author: Nick Schuch
-
-DIR='/var/www/results/puppet'
-
-cd $DIR && sh scripts/provision.sh
